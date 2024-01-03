31d48f60b5b31c26a8e0215ab7586289afb4bab9
sandeepRoy <sandeep.roy2014@gmail.com>
2024-01-03 12:55:54 +0530

    Spring Setup

---
+++ SpeerBackEnd/src/main/java/com/speer/SpeerBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.speer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeerBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeerBackEndApplication.class, args);
+    }
+
+}
---
+++ SpeerBackEnd/src/test/java/com/speer/SpeerBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeerBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
